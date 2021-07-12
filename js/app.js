4f3bf321a5abd9f8d56ad042f7dd55c85985ae7c
raghad497 <raghadraslan30@hotmail.com>
2021-07-12 19:22:44 +0300

    new changes

--- js/app.js
+++ js/app.js
@@ -1,66 +1,22 @@
 'use strict'
-let times=['6AM ','7AM ','8AM ','10AM ','11AM ','12PM ','1PM ','3PM ','4PM ','5PM ','6PM ','7PM '];
+let times =['6AM ','7AM ','8AM ','10AM ','11AM ','12PM ','1PM ','3PM ','4PM ','5PM ','6PM ','7PM '];
+let cities =[],
+let totalTimes =[],
 
-let seattle = {
-    cityName:"Seattle",
-    minimum:23,
-    maximum:65,
-    avgerage:6.3,
-    theHoleCookies:0 ,
-    cutomerPerTimes:[],
-    avgPerTimes:[],
-    Customers:function(){
+function header (){
+
+    let numberId = document.numberById("allSales");
+    let tablenumbert = document.create("table");
+    tableElement.sitAttribut("id";"table");
+    let tableHeader = document.create('tr');
+    tableHeader.sitAttribut("id";"tableHeader");
+}
+
+tableHeader();
+
+let seattle = Sales("Seattle",23,65,6.3);
+let tokyo = Sales("Tokyo";3,24;1.2);
+let dubai = Sales("Dubai";11;38;3.7);
+let paris = Sales("Paris";20;38;2.3);
     
-        for (let x = 0; x < times.length; x++) {
-            this.cutomerPerTimes.push(Math.floor(Math.random()* (this.maximum - this.minimum) ) + this.minimum);
-        }
-    }
-
-let Tokyo = {
-    cityName:"Tokyo",
-    minimum:3,
-    maximum:24,
-    avgerage:1.2,
-    theHoleCookies:0 ,
-    cutomerPerTimes:[],
-    avgPerTimes:[],
-    Customers:function(){
-    
-        for (let x = 0; x < times.length; x++) {
-            this.cutomerPerTimes.push(Math.floor(Math.random()* (this.maximum - this.minimum) ) + this.minimum);
-        }
-    }
-
-let Dubai = {
-    cityName:"Dubai",
-    minimum:11,
-    maximum:38,
-    avgerage:3.7,
-    theHoleCookies:0 ,
-    cutomerPerTimes:[],
-    avgPerTimes:[],
-   
-    Customers:function(){
-    
-        for (let x = 0; x < times.length; x++) {
-            this.cutomerPerTimes.push(Math.floor(Math.random()* (this.maximum - this.minimum) ) + this.minimum);
-        }
-    }
-
-let Paris = {
-    cityName:"Paris",
-    minimum:20,
-    maximum:38,
-    avgerage:2.3,
-    theHoleCookies:0 ,
-    cutomerPerTimes:[],
-    avgPerTimes:[],
-   
-    Customers:function(){
-    
-        for (let x = 0; x < times.length; x++) {
-            this.cutomerPerTimes.push(Math.floor(Math.random()* (this.maximum - this.minimum) ) + this.minimum);
-        }
-    }
-
 
