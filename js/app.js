80c6bcb0eba41b379829ae601cb70e7ea1f4abaf
raghad497 <raghadraslan30@hotmail.com>
2021-07-12 20:25:34 +0300

    new changes

--- js/app.js
+++ js/app.js
@@ -3,20 +3,20 @@ let times =['6AM ','7AM ','8AM ','10AM ','11AM ','12PM ','1PM ','3PM ','4PM ','5
 let cities =[],
 let totalTimes =[],
 
-function header (){
-
-    let numberId = document.numberById("allSales");
-    let tablenumbert = document.create("table");
-    tableElement.sitAttribut("id";"table");
-    let tableHeader = document.create('tr');
-    tableHeader.sitAttribut("id";"tableHeader");
-}
-
 tableHeader();
 
-let seattle = Sales("Seattle",23,65,6.3);
-let tokyo = Sales("Tokyo";3,24;1.2);
-let dubai = Sales("Dubai";11;38;3.7);
-let paris = Sales("Paris";20;38;2.3);
+let seattle = Sales("Seattle")
+let tokyo = Sales("Tokyo")
+let dubai = Sales("Dubai")
+let paris = Sales("Paris")
+
+function header (){
+let numberId = document.numberId("allSales");
+let tableNumber=document.create("table");
+ let tableHeader = document.creat("tr")
+  tableHeader.sitAttribut("Id";"tableHeader");
+}
+
+
     
 
